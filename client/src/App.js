3458f19a9a42d1851f287e05e4b4bd8568fc6d5f
Agustina Monzón <agusdlmonzon@gmail.com>
2022-12-10 23:00:49 -0300

    se agregaron componentes, slices, actions y se cambiaron las dependencias

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
-import "./App.css";
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from './components/Home/Home';
+import Menu from './components/Menu/Menu';
+import News from './components/News/News';
+import Locations from './components/Locations/Locations';
+import Delivery from './components/Delivery/Delivery';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Mc Burger</h1>
-    </div>
+    <BrowserRouter>
+       <Route exact path="/" component={Home} />
+       <Route exact path="/menu" component={Menu} />
+       <Route exact path="/news" component={News} />
+       <Route exact path="/locations" component={Locations} />
+       <Route exact path="/delivery" component={Delivery} />
+     </BrowserRouter>
   );
 }
 
