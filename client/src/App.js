2895d226f7ca732abcde8a89a789f1db048e8e07
Frankroz <franjrm20119@gmail.com>
2022-12-10 20:41:36 -0500

    Crear componente details con su ruta

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import Home from './components/Home/Home';
-import Menu from './components/Menu/Menu';
-import News from './components/News/News';
-import Locations from './components/Locations/Locations';
-import Delivery from './components/Delivery/Delivery';
+import Home from "./components/Home/Home";
+import Menu from "./components/Menu/Menu";
+import News from "./components/News/News";
+import Locations from "./components/Locations/Locations";
+import Delivery from "./components/Delivery/Delivery";
+import Details from "./components/Details/Details";
 
 function App() {
   return (
     <BrowserRouter>
-       <Route exact path="/" component={Home} />
-       <Route exact path="/menu" component={Menu} />
-       <Route exact path="/news" component={News} />
-       <Route exact path="/locations" component={Locations} />
-       <Route exact path="/delivery" component={Delivery} />
-     </BrowserRouter>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/menu" component={Menu} />
+      <Route exact path="/news" component={News} />
+      <Route exact path="/locations" component={Locations} />
+      <Route exact path="/delivery" component={Delivery} />
+      <Route exact path="/product/:id" component={Details} />
+    </BrowserRouter>
   );
 }
 
