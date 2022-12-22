f2b2c4d0463eccbab13111d7429a2f26693fd616
Frankroz <franjrm20119@gmail.com>
2022-12-22 17:57:02 -0500

    Cart component functional with /payment route

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -8,7 +8,7 @@ import {
   ADD_TO_CART,
   RESTART_CART,
   REMOVE_FROM_CART,
-  DELETE_PRODUCTS_CART
+  DELETE_PRODUCTS_CART,
 } from "../actions/index";
 
 const initialState = {
@@ -16,37 +16,19 @@ const initialState = {
   products: [],
   productsCategory: [],
   product: {},
-  cart: {
-    1: {
-      id: 1,
-      name: 'Cheeseburger',
-      image: 'asdf.com',
-      price: "$2",
-      quantity: 2
-    },
-    2: {
-      id: 2,
-      name: 'Chicken Nuggets',
-      image: 'asdf.com',
-      price: "$2",
-      quantity: 3
-    }
-  },
+  cart: {},
 };
 
 function rootReducer(state = initialState, action) {
-
   let newCart = { ...state.cart };
 
   switch (action.type) {
-
     case GET_ALL_PRODUCTS:
       return {
         ...state,
         products: action.payload,
         allProducts: action.payload,
       };
-
 
     case GET_PRODUCTS_ID:
       return {
@@ -55,32 +37,38 @@ function rootReducer(state = initialState, action) {
       };
 
     case GET_PRODUCTS_CATEGORY:
-
       return {
         ...state,
-        productsCategory: state.allProducts.filter((p) => p.idCategory === action.payload),
-        products: state.allProducts.filter((p) => p.idCategory === action.payload),
+        productsCategory: state.allProducts.filter(
+          (p) => p.idCategory === action.payload
+        ),
+        products: state.allProducts.filter(
+          (p) => p.idCategory === action.payload
+        ),
       };
 
     case GET_PRODUCTS_BY_INGREDIENT:
       //recibe uno o muchos "productIngredients"
-      const products = state.productsCategory //los que estan cargados
-      const searchIngredients = action.payload //los que envio
+      const products = state.productsCategory; //los que estan cargados
+      const searchIngredients = action.payload; //los que envio
 
       return {
         ...state,
-        products: action.payload === ["All"] ? state.productsCategory : products.filter((p) => {
-          let productIngredients = p.ingredients
-          productIngredients = productIngredients.split("-");
-          productIngredients = productIngredients.map(e => e.trim());
-          let coincidense = false
-          searchIngredients.map(e =>
-            productIngredients.map(i => {
-              if (e === i) coincidense = true;
-            })
-          )
-          return coincidense;
-        })
+        products:
+          action.payload === ["All"]
+            ? state.productsCategory
+            : products.filter((p) => {
+                let productIngredients = p.ingredients;
+                productIngredients = productIngredients.split("-");
+                productIngredients = productIngredients.map((e) => e.trim());
+                let coincidense = false;
+                searchIngredients.map((e) =>
+                  productIngredients.map((i) => {
+                    if (e === i) coincidense = true;
+                  })
+                );
+                return coincidense;
+              }),
       };
 
     case GET_PRODUCTS_AMOUNT:
@@ -96,23 +84,22 @@ function rootReducer(state = initialState, action) {
       };
 
     case ORDER_BY_PRICE:
-      let orderArray = [...state.products]
-      action.payload == 'asc' ?
-        orderArray.sort(function (a, b) {
-          a = a.price.split("$")[1]
-          b = b.price.split("$")[1]
-          return parseInt(b) - parseInt(a);
-
-        }) : orderArray.sort(function (a, b) {
-          a = a.price.split("$")[1]
-          b = b.price.split("$")[1]
-          return parseInt(a) - parseInt(b);
-        })
+      let orderArray = [...state.products];
+      action.payload == "asc"
+        ? orderArray.sort(function (a, b) {
+            a = a.price.split("$")[1];
+            b = b.price.split("$")[1];
+            return parseInt(b) - parseInt(a);
+          })
+        : orderArray.sort(function (a, b) {
+            a = a.price.split("$")[1];
+            b = b.price.split("$")[1];
+            return parseInt(a) - parseInt(b);
+          });
       return {
         ...state,
         products: orderArray,
-      }
-
+      };
 
     case ADD_TO_CART:
       if (state.cart.hasOwnProperty(action.payload.id)) {
@@ -130,8 +117,8 @@ function rootReducer(state = initialState, action) {
     case RESTART_CART:
       return {
         ...state,
-        cart: {}
-      }
+        cart: {},
+      };
 
     case REMOVE_FROM_CART:
       if (state.cart.hasOwnProperty(action.payload.id)) {
@@ -145,11 +132,11 @@ function rootReducer(state = initialState, action) {
       return { ...state, cart: newCart };
 
     case DELETE_PRODUCTS_CART:
-      const prodCart = state.cart;
-      delete prodCart[action.payload.id]
+      delete newCart[action.payload.id];
+
       return {
         ...state,
-        cart: prodCart
+        cart: newCart,
       };
 
     default:
